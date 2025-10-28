c14d909051fbd6c638b0fc4b47d0370a18339986
Camilly <133771406+camillycampelo@users.noreply.github.com>
2025-10-28 12:30:33 -0300

    Fix router inclusion and add pagination support
    Fixed syntax error in include_router method and added global pagination support.

--- workout_api/main.py
+++ workout_api/main.py
@@ -2,4 +2,7 @@ from fastapi import FastAPI
 from workout_api.routers import api_router
 
 app = FastAPI(title='WorkoutApi')
-app.include_router(api_router)
+app.include_router(api_routerprefix="/atletas", tags=["Atletas"])
+
+# 🧠 Adiciona suporte global à paginação
+add_pagination(app)
